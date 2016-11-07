b8bd18ed5d6d3b0e65b0efbdda12d0798ebb98e0
huangning <huangning@huangning-PC>
2016-11-07 14:14:31 +0800

    get branch

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+'''
+Created on 2016Äê11ÔÂ2ÈÕ
+
+@author: huangning
+'''
---
+++ app/headlines.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+'''
+Created on 2016��11��2��
+
+@author: huangning
+'''
+from flask import Flask ,render_template,request
+import feedparser
+
+
+app = Flask(__name__)
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+'xinhuafangchan': 'http://www.xinhuanet.com/house/news_house.xml',
+'fox': 'http://feeds.foxnews.com/foxnews/latest',
+'tecent_news': 'http://news.qq.com/newsgn/rss_newsgn.xml'}
+
+@app.route("/")
+
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
+        
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    #print len(feed)
+    #print feed
+    #first_article = feed['entries'][0]
+    return render_template("home.html",articles=feed['entries'])
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
